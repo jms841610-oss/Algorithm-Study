37b1178b22fec5951a1614ab654222d4e89a3300
jms841610-oss <jms841610@gmail.com>
2026-02-18 00:09:01 +0900

    [Silver II] Title: 좌표 압축, Time: 420 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,33 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <map>
 
 using namespace std;
 
-int N;
-
-vector<int> sorted_coordinate;
-map<int,int> compression;
-
-void compressing(){
-    int comulative_sum = 0;
-
-    sort(sorted_coordinate.begin(),sorted_coordinate.end());
-    
-    compression[sorted_coordinate[0]] = comulative_sum;
-
-    for(int i=1;i<N;i++){
-        if(sorted_coordinate[i] > sorted_coordinate[i-1]){
-            comulative_sum++;
-            compression[sorted_coordinate[i]] = comulative_sum;
-        }
-    }
-    return;
-}
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
     cin >> N;
     
+    vector<int> sorted_coordinate;
     vector<int> origin_coordinate;
 
     sorted_coordinate.reserve(N);
@@ -39,13 +23,15 @@ int main(){
         sorted_coordinate.push_back(x);
         origin_coordinate.push_back(x);
     }
-    compressing();
+    sort(sorted_coordinate.begin(),sorted_coordinate.end());
 
+    auto last = unique(sorted_coordinate.begin(),sorted_coordinate.end());
+
+    sorted_coordinate.erase(last,sorted_coordinate.end());
+    
     for(int j=0;j<N;j++){
-        int result = compression[origin_coordinate[j]];
-        cout << result << " ";
+        int idx = lower_bound(sorted_coordinate.begin(),sorted_coordinate.end(),origin_coordinate[j]) - sorted_coordinate.begin();
+        cout << idx << " ";
     }
-    cout << endl;
-
     return 0;
 }
