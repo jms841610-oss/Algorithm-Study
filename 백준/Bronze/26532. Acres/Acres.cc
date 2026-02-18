7f3e08e19f13a1f43a3f2139fa8a91f71fae5a45
JO MINSUNG <jms841610@gmail.com>
2026-02-18 23:42:59 +0900

    Update Acres.cc

--- 백준/Bronze/26532. Acres/Acres.cc
+++ 백준/Bronze/26532. Acres/Acres.cc
@@ -1,13 +1,9 @@
-/**
- * @file main.cpp
+/*
  * @brief 면적 계산 및 필요 물품 개수 구하기 (Ceiling Division)
  * @details
  * - 입력받은 가로(X), 세로(Y)를 이용해 전체 면적을 구하고,
  * - 특정 단위(one_bag)로 나누었을 때 필요한 최소 개수를 구하는 로직.
  * - 핵심: 나눗셈의 나머지가 있을 때 무조건 올림 처리하는 정수 연산 테크닉 사용.
- *
- * @author (작성자 성함)
- * @date 2026-02-18
  */
 
 #include <iostream>
@@ -25,7 +21,7 @@ int main() {
     // 따라서 안전하게 64비트 정수형인 long long을 사용함.
     long long X, Y;
 
-    if (!(cin >> X >> Y)) return 0; // 입력 예외 처리 (선택 사항)
+    cin >> X >> Y;
 
     // [면적 계산]
     // X와 Y가 큰 경우, 결과가 int를 넘을 수 있으므로 area 변수도 long long이어야 함.
