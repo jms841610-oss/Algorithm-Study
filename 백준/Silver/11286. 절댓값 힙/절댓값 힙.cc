b3ed724bc4000d7035e97cc2ae435ffeb5126736
JO MIN SUNG <jms841610@gmail.com>
2026-02-22 23:55:19 +0900

    [Silver I] Title: 절댓값 힙, Time: 548 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+struct absolute_smaller{
+    bool operator()(const int a,const int b) const{
+        if(abs(a)==abs(b)){
+            return a>b;
+        }
+        return abs(a) > abs(b);
+    }
+};
+int main(){
+    int N;
+    cin >> N;
+
+    priority_queue<int,vector<int>,absolute_smaller> pq;
+
+    for(int i=0;i<N;i++){
+        int x;
+        cin >> x;
+        if(x==0){
+            if(!pq.empty()){
+                int top_val = pq.top();
+                pq.pop();
+                cout << top_val << "\n";
+            }else{
+                cout << 0 << "\n";
+            }
+        }else{
+            pq.push(x);
+        }
+    }
+    return 0;
+}
