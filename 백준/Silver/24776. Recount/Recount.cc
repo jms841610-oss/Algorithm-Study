6c43a3778b8de8799ed030d3daafd82f883dab77
JO MIN SUNG <jms841610@gmail.com>
2026-02-27 00:29:13 +0900

    [Silver V] Title: Recount, Time: 64 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/24776. Recount/Recount.cc
+++ 백준/Silver/24776. Recount/Recount.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <string>
 
 using namespace std;
@@ -8,7 +8,7 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    map<string,int> votes;
+    unordered_map<string,int> votes;
 
     int max_votes = 0;
 
