06c6eb29027ac50da2b4b01037bed57869de58ab
JO MIN SUNG <jms841610@gmail.com>
2026-02-27 00:24:50 +0900

    [Silver V] Title: Recount, Time: 108 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/24776. Recount/Recount.cc
+++ 백준/Silver/24776. Recount/Recount.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <map>
-#include <vector>
 #include <string>
 
 using namespace std;
@@ -10,7 +9,6 @@ int main(){
     cin.tie(NULL);
 
     map<string,int> votes;
-    vector<string> possible;
 
     int max_votes = 0;
 
@@ -21,22 +19,18 @@ int main(){
 
         if(name == "***") break;
 
-        if(votes.find(name)!=votes.end()){
-            votes[name]++;
-        }else{
-            votes[name] = 0;
-            possible.push_back(name);
-        }
+        votes[name]++;
+
         max_votes = max(max_votes,votes[name]);
     }
     string candidate = "";
 
     int cnt = 0;
 
-    for(int i=0;i<possible.size();i++){
-        if(votes[possible[i]]==max_votes){
+    for(const auto &pair : votes){
+        if(pair.second==max_votes){
+            candidate = pair.first;
             cnt++;
-            candidate = possible[i];
         }
     }
     if(cnt==1){
