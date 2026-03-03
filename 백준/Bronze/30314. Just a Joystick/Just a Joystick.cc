bd90861d3526efb2b438d9b77c216d2b3a243006
JO MIN SUNG <jms841610@gmail.com>
2026-03-04 03:52:57 +0900

    Update Just a Joystick.cc

--- 백준/Bronze/30314. Just a Joystick/Just a Joystick.cc
+++ 백준/Bronze/30314. Just a Joystick/Just a Joystick.cc
@@ -1,25 +1,31 @@
 #include <iostream>
+#include <string>
+#include <cmath>     // abs() 함수 사용을 위해 추가
+#include <algorithm> // min() 함수 사용을 위해 추가
 
 using namespace std;
 
-int main(){
+int main() {
+    // 입출력 속도 향상을 위한 C++ 최적화 코드 (선택 사항)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n;
     cin >> n;
 
-    string winner;
-    cin >> winner;
-
-    string mine;
-    cin >> mine;
+    string winner, mine;
+    cin >> winner >> mine;
 
     int cost_sum = 0;
-    int cost = 0;
 
-    for(int i=0;i<n;i++){
-        if(winner[i] > mine[i]) cost = (winner[i] - mine[i]) < (26 - (winner[i] - mine[i])) ? (winner[i] - mine[i]) : (26 - (winner[i] - mine[i]));
-        else                    cost = (mine[i] - winner[i]) < (26 - (mine[i] - winner[i])) ? (mine[i] - winner[i]) : (26 - (mine[i] - winner[i]));
-        cost_sum += cost;
+    for (int i = 0; i < n; i++) {
+        // 1. 두 문자의 아스키코드 차이(절대값) 계산
+        int diff = abs(winner[i] - mine[i]);
+        
+        // 2. 정방향 거리(diff)와 역방향 거리(26 - diff) 중 최소값을 누적
+        cost_sum += min(diff, 26 - diff);
     }
+
     cout << cost_sum << "\n";
 
     return 0;
