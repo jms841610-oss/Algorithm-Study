6d70358e1a7331003e847af0ae5014c918336ad2
JO MIN SUNG <jms841610@gmail.com>
2026-03-19 00:14:12 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,113 +1,25 @@
 #include <iostream>
-#include <vector> 
+#include <vector>
 
 using namespace std;
 
-int N,M;
+int N, M;
 
-void one_solve(){
-    for(int i=1;i<=N;i++){
-            cout << i << "\n";
-    }
-    return;
-}
-void two_solve(){
-    for(int i=1;i<=N;i++){
-        for(int j=i+1;j<=N;j++){
-            cout << i << " " << j << "\n";
+vector<int> arr;
+
+void solve(int depth,int start){
+    if(depth==M){
+        for(int i=0;i<M;i++){
+            cout << arr[i] << " ";
         }
-    }  
-    return;  
-}
-void three_solve(){
-    for(int i=1;i<=N;i++){
-        for(int j=i+1;j<=N;j++){
-            for(int k=j+1;k<=N;k++){
-                cout << i << " " << j << " " << k << "\n";
-            }
-        }
+        cout << "\n";
+        return;
     }
-    return;
-}
-void four_solve(){
-    for(int i=1;i<=N;i++){
-        for(int j=i+1;j<=N;j++){
-            for(int k=j+1;k<=N;k++){
-                for(int n=k+1;n<=N;n++){
-                    cout << i << " " << j << " " << k << " " << n << "\n";
-                }
-            }
-        }
+    for(int i=start;i<=N;i++){
+        arr.push_back(i);
+        solve(depth+1,i+1);
+        arr.pop_back();
     }
-    return;
-}
-void five_solve(){
-    for(int i=1;i<=N;i++){
-        for(int j=i+1;j<=N;j++){
-            for(int k=j+1;k<=N;k++){
-                for(int n=k+1;n<=N;n++){
-                    for(int m=n+1;m<=N;m++){
-                        cout << i << " " << j << " " << k << " " << n << " " << m << "\n";
-                    }
-                }
-            }
-        }
-    }
-    return;
-}
-void six_solve(){
-    for(int i=1;i<=N;i++){
-        for(int j=i+1;j<=N;j++){
-            for(int k=j+1;k<=N;k++){
-                for(int n=k+1;n<=N;n++){
-                    for(int m=n+1;m<=N;m++){
-                        for(int a=m+1;a<=N;a++){
-                            cout << i << " " << j << " " << k << " " << n << " " << m << " " << a << "\n";
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return;
-}
-void seven_solve(){
-    for(int i=1;i<=N;i++){
-        for(int j=i+1;j<=N;j++){
-            for(int k=j+1;k<=N;k++){
-                for(int n=k+1;n<=N;n++){
-                    for(int m=n+1;m<=N;m++){
-                        for(int a=m+1;a<=N;a++){
-                            for(int b=a+1;b<=N;b++){
-                                cout << i << " " << j << " " << k << " " << n << " " << m << " " << a << " " << b << "\n";
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return;
-}
-void eight_solve(){
-    for(int i=1;i<=N;i++){
-        for(int j=i+1;j<=N;j++){
-            for(int k=j+1;k<=N;k++){
-                for(int n=k+1;n<=N;n++){
-                    for(int m=n+1;m<=N;m++){
-                        for(int a=m+1;a<=N;a++){
-                            for(int b=a+1;b<=N;b++){
-                                for(int c=b+1;c<=N;c++){
-                                    cout << i << " " << j << " " << k << " " << n << " " << m << " " << a << " " << b << " " << c << "\n";
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return;
 }
 int main(){
     ios::sync_with_stdio(false);
@@ -115,14 +27,7 @@ int main(){
 
     cin >> N >> M;
 
-    if(M==1) one_solve();
-    if(M==2) two_solve();
-    if(M==3) three_solve();
-    if(M==4) four_solve();
-    if(M==5) five_solve();
-    if(M==6) six_solve();
-    if(M==7) seven_solve();
-    if(M==8) eight_solve();
+    solve(0,1);
 
     return 0;
 }
