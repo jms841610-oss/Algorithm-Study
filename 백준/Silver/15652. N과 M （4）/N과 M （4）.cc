ac3677ed980772b3d834b2df5deaf201e7b85a81
JO MIN SUNG <jms841610@gmail.com>
2026-03-19 22:55:45 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N,M;
+vector<int> arr;
+
+void solve(int depth,int start){
+    if(depth==M){
+        for(int i=0;i<arr.size();i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i=start;i<=N;i++){
+        arr.push_back(i);
+        solve(depth+1,i);
+        arr.pop_back();
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M;
+
+    solve(0,1);
+
+    return 0;
+}
