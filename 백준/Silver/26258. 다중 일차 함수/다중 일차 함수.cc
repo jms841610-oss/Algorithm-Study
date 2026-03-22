6833e476312ba25d77ccf25ff5e0a2177464ceab
JO MIN SUNG <jms841610@gmail.com>
2026-03-22 20:21:14 +0900

    Update 다중 일차 함수.cc

--- 백준/Silver/26258. 다중 일차 함수/다중 일차 함수.cc
+++ 백준/Silver/26258. 다중 일차 함수/다중 일차 함수.cc
@@ -9,7 +9,7 @@ int main(){
     cin.tie(NULL);
 
     int N;
-    cin >> N;
+    if (!(cin >> N)) return 0;
 
     vector<long long> x(N);
     vector<long long> y(N);
@@ -17,6 +17,8 @@ int main(){
     for(int i=0;i<N;i++){
         cin >> x[i] >> y[i];
     }
+    
+    // slope 배열의 크기는 N-1 입니다.
     vector<int> slope(N-1);
 
     for(int i=0;i<N-1;i++){
@@ -28,6 +30,7 @@ int main(){
             slope[i] = 0;
         }
     }
+    
     int Q;
     cin >> Q;
 
@@ -35,10 +38,17 @@ int main(){
         double query;
         cin >> query;
 
-        auto it = lower_bound(x.begin(),x.end(),query);
-        int idx = it - x.begin() - 1;
+        // query보다 엄격하게 큰 첫 번째 원소의 반복자를 찾습니다.
+        auto it = upper_bound(x.begin(), x.end(), query);
         
-        cout << slope[idx] << "\n";
+        // it가 x.begin()이거나 x.end()이면 유효한 구간을 벗어난 것입니다.
+        if(it != x.begin() && it != x.end()){
+            // 올바른 구간의 인덱스를 얻기 위해 1을 빼줍니다.
+            int idx = (it - x.begin()) - 1; 
+            
+            // 이제 안전하게 slope 배열에 접근할 수 있습니다.
+            cout << slope[idx] << "\n";
+        }
     }
     return 0;
 }
