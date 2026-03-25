3bd330c755c8a788f2a55bca771ab3eebd1499b0
JO MIN SUNG <jms841610@gmail.com>
2026-03-26 06:11:58 +0900

    [Gold V] Title: 평범한 배낭, Time: 40 ms, Memory: 42004 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<int>> dp(N+1,vector<int>(M+1,0));
+    vector<pair<int,int>> arr(N+1);
+
+    arr[0] = {0,0};
+
+    for(int i=1;i<=N;i++){
+        cin >> arr[i].first >> arr[i].second;
+    }
+    for(int i=1;i<=N;i++){
+        int now_wei = arr[i].first;
+        int now_val = arr[i].second;
+
+        for(int j=1;j<=M;j++){
+            if(now_wei <= j){
+                dp[i][j] = max(dp[i-1][j], (dp[i-1][j-now_wei] + now_val));
+            }else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    int ans = 0;
+
+    for(int i=1;i<=M;i++){
+        ans = max(ans, dp[N][i]);
+    }
+    cout << ans << "\n";
+    return 0;
+}
