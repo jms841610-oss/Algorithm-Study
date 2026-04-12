8a1baed2f668dae5305e0a49d4d7daddcb15dc0d
JO MIN SUNG <jms841610@gmail.com>
2026-04-12 21:02:25 +0900

    Update 의상.cpp

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,22 +1,24 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <set>
 
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    int answer = 1;
+    int answer = 1; // 곱셈을 위해 1로 초기화합니다.
     
+    // 1단계: 해시맵을 이용해 종류별 옷의 개수를 셉니다.
     unordered_map<string, int> counts;
-    set<string> types;
+    for(const vector<string>& cloth : clothes){
+        // cloth[1]은 옷의 종류(type)를 의미합니다.
+        counts[cloth[1]]++; 
+    }
     
-    for(const vector<string> &cloth : clothes){
-        counts[cloth[1]]++;
-        types.insert(cloth[1]);
+    // 2단계: 각 옷의 종류별로 (옷의 개수 + 안 입는 경우 1)을 곱해줍니다.
+    for(auto const& [type, count] : counts){
+        answer *= (count + 1);
     }
-    for(const string &type : types){
-        answer *= counts[type]+1;
-    }
-    return answer-1;
+    
+    // 3단계: 모든 종류의 옷을 하나도 입지 않은 경우(1가지)를 빼줍니다.
+    return answer - 1;
 }
